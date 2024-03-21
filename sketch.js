d5cd27b4add976a0a51538bf73c24a45511c21d1
João Lúcio <jpedro.boavida@gmail.com>
2024-03-21 14:41:53 +0000

    1

--- sketch.js
+++ sketch.js
@@ -1,11 +1,27 @@
+let diam = 200
+
 function setup() {
-  createCanvas(1280, 1024);
-  background(153);
-  stroke(0);
-  strokeWeight(2);
-  fill(237, 28, 36);
-  circle(30, 30, 40);
-  line(30, 30, 1280 / 2, 1024 / 2);
+  createCanvas(800, 800);
+  background(0)
 }
 
-function draw() {}
+function draw() {
+  stroke(255)
+  strokeWeight(5)
+  circle(width/2, height/2, diam)
+  if(mouseX > width/2){
+    
+    if(mouseY > height/2)
+    fill(255)
+    else
+    fill(0,255,0)
+    
+ }
+  
+  else{
+   if(mouseY >height/2)
+     fill(0,0,255)
+  else
+    fill(255,0,0)
+  }
+}
